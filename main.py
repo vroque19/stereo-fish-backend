93649111498b19ca9e9e663354dbe58bb5e8b5d9
Vanessa Roque <viroque19@gmail.com>
2025-04-14 23:42:01 +0000

    update main

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI, File, UploadFile, Header, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
-from typing import Annotated
+from typing import Annotated, List
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 app = FastAPI()
-SECRET = "secret-key"
-
-
+SECRET = os.environ.get("SECRET_KEY")
+UPLOAD_DIRECTORY = "./static/uploaded_images"
+os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
 
-# Dependency to check token
 def verify_token(authorization: Annotated[str | None, Header()] = None):
     print(f"Received Authorization header: {authorization}")
     if authorization != f"Bearer {SECRET}":
@@ -22,20 +23,31 @@ def verify_token(authorization: Annotated[str | None, Header()] = None):
             headers={"WWW-Authenticate": "Bearer"},
         )
     return True
+# @app.get("/whoami")
+# async def read_items(user_agent: Annotated[str | None, Header()] = None,
+#     _auth: bool = Depends(verify_token)):
+#    return {"User agent": user_agent}
+
+
+# query parameter <-- less secure
 @app.get("/whoami")
-async def read_items(user_agent: Annotated[str | None, Header()] = None,
-    _auth: bool = Depends(verify_token)):
-   return {"User agent": user_agent}
+async def whoami(user_agent: Annotated[str | None, Header()] = None,
+                 _auth: bool = Depends(verify_token)):
+    return {"User agent": user_agent}
 
 
 @app.post("/upload-image/")
-async def upload_image(file: UploadFile = File(...)):
+async def upload_image(file: UploadFile = File(...), _auth: bool = Depends(verify_token)):
     if not file.content_type.startswith("image/"):
-        return JSONResponse(content={"error": "File is not an image."}, status_code=400)
+        raise HTTPException(status_code=415, detail="Unsupported media type.")
 
-    # Save the file (optional)
+    # if file is larger than 1GB
+    if file.size > 10**9:
+       raise HTTPException(status_code=413, detail="Payload too large")
+    
+    # Save the file
     contents = await file.read()
-    with open(f"uploaded_images/{file.filename}", "wb") as f:
+    with open(f"static/uploaded_images/{file.filename}", "wb") as f:
         f.write(contents)
 
     return {"filename": file.filename, "content_type": file.content_type}
